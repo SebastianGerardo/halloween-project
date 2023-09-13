b551f0e93ccd3424c933350d0da5b217bc4ae589
Sebastián Gerardo <sebastiangerardo27@gmail.com>
2023-09-13 18:35:33 -0500

    SG | suscribirse

--- src/pages/Home/components/NuevosItems/NuevosItems.jsx
+++ src/pages/Home/components/NuevosItems/NuevosItems.jsx
@@ -35,18 +35,18 @@ export const NuevosItems = () => {
                 <div className="tw-absolute tw-top-[8%] tw-left-[8%] tw-bg-[#f4511f] tw-text-white tw-text-[.813rem] 
                                 large:tw-text-[.875rem] tw-py-1 tw-px-[.05rem] tw-rounded-sm">{slide.tag}</div>
                 <img src={slide.image} alt="" className="tw-w-[120px] tw-mb-2 tw-transition tw-duration-[.3s] large:tw-w-[150px] large:tw-mb-4 tw-mx-auto group-hover:-tw-translate-y-2" />
-                <h3 className="new__title">{slide.title}</h3>
-                <span className="new__subtitle">{slide.subtitle}</span>
+                <h3 className="tw-text-[.938rem] large:tw-text-[1.25rem] tw-font-medium">{slide.title}</h3>
+                <span className="tw-block tw-text-[.813rem] tw-mb-[.5rem] large:tw-mb-[1rem]">{slide.subtitle}</span>
 
-                <div className="new__prices">
-                  <span className="new__price">{slide.price}</span>
-                  <span className="new__discount">{slide.discount}</span>
+                <div className="tw-inline-flex tw-items-center tw-gap-2">
+                  <span className="tw-font-medium tw-text-white">{slide.price}</span>
+                  <span className="tw-text-[#F4511F] tw-text-[.75rem] tw-line-through tw-font-medium">{slide.discount}</span>
                 </div>
 
                 <button className="tw-inline-flex tw-bg-[#F4511F] hover:tw-bg-[#F4420B] tw-text-white 
                                    tw-p-2 tw-absolute tw-rounded-[.25rem_.25rem_.75rem_.25rem] tw-w-9 tw-h-9 tw-bottom-0 
                                    -tw-right-12 tw-transition tw-duration-[.3s] group-hover:-tw-translate-x-12 new__button">
-                  <i className="bx bx-cart-alt new__icon"></i>
+                  <i className="bx bx-cart-alt tw-text-[1.25rem]"></i>
                 </button>
               </div>
             ))}
--- src/pages/Home/components/Suscribirse/Suscribirse.jsx
+++ src/pages/Home/components/Suscribirse/Suscribirse.jsx
@@ -2,20 +2,33 @@
 import "./suscribirse.css";
 
 export const Suscribirse = () => (
-  <section className="section newsletter">
-    <div className="newsletter__container container">
-      <h2 className="section__title">Newsletter</h2>
-      <p className="newsletter__description">
+  <section className="tw-pt-[4.5rem] tw-pr-0 tw-pb-8 tablet:tw-pt-[7rem] tablet:tw-pr-0 tablet:tw-pb-8">
+    <div
+      className="newsletter__container medium:tw-grid medium:tw-grid-cols-[.7fr] medium:tw-justify-center
+                    tablet:tw-grid-cols-[.5fr] tw-max-w-[968px] tw-mx-6 max-[320px]:tw-mx-[1rem] large:tw-mx-auto"
+    >
+      <h2 className="large:tw-text-[2.25rem] tw-font-semibold large:tw-mb-12 tw-text-[1.25rem] tw-mb-8 tw-text-center">
+        Newsletter
+      </h2>
+      <p className="tw-text-center tw-mb-6 medium:tw-py-0 medium:tw-px-12">
         Promotion new products and sales. Directly to your inbox
       </p>
 
-      <form action="" className="newsletter__form">
+      <form
+        action=""
+        className="newsletter__form-bg tw-p-4 tw-flex tw-justify-between tw-rounded-xl"
+      >
         <input
           type="text"
           placeholder="Enter your email"
-          className="newsletter__input"
+          className="tw-w-[70%] tw-bg-transparent tw-text-white"
         />
-        <button className="button">Subscribe</button>
+        <button
+          className="tw-inline-block tw-bg-[#F4511F] hover:tw-bg-[#F4420B] tw-text-white tw-py-4 tw-px-[1.75rem] 
+                           tw-rounded-lg tw-font-medium tw-transition tw-duration-[.3s]"
+        >
+          Subscribe
+        </button>
       </form>
     </div>
   </section>
